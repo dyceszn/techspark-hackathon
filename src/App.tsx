17b2decb445ee5e0afb1b095f161f581cb569e6f
Olabalade Emmanuel <olabaladeemmanuel@gmail.com>
2025-04-11 20:37:10 +0100

    Create signin and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
