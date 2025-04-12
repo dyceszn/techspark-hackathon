3e161aaaf7038d8450c39ea06989c24924bdcb31
Duruoha Ikenna <duruohaikenna@gmail.com>
2025-04-12 09:07:09 +0100

    Add Service UI

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -4,3 +4,5 @@ export { default as back } from "./left-arrow.svg";
 export { default as edit } from "./edit-text.png";
 export { default as nav } from "./left.svg";
 export { default as cancel } from "./cancel.svg";
+export { default as cart } from "./cart.svg";
+export { default as comment } from "./chat-teardrop-dots.svg";
--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Back, Footer, Header } from "../components";
-import { back } from "../assets/icons";
+import { back, cart, comment } from "../assets/icons";
 
 const Service = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col">
       <Header />
       <Back />
 
-      <div className="border-black border-1 px-[10vw] h-[78vh] flex">
-        <div className="border-black border-1 w-[50%] h-full flex flex-col justify-between">
-          <div className="border-black border-1 h-[15%] flex flex-col justify-between">
+      <div className=" px-[10vw] flex-grow flex justify-between">
+        <div className=" w-[50%] flex flex-col justify-between h-[90%]">
+          <div className=" h-[18%] flex flex-col justify-between">
             <p className="text-2xl font-[Lexend] font-semibold">
-              From basic to bold. Let’s make your nails stand out.
+              {"From basic to bold. Let’s make your nails stand out."}
             </p>
 
             <div className=" flex flex-row items-center">
@@ -21,20 +21,23 @@ const Service = () => {
                 alt=""
                 className="size-8 bg-[var(--tertiary-color)] rounded-full"
               />
-              <p className="pl-6">Shalom Jamime</p>
+              <p className="pl-6">{"Shalom Jamime"}</p>
             </div>
 
             <div className=" flex flex-row items-center">
               <p className="px-6 py-2 rounded-full bg-[var(--tertiary-color)]">
-                Beauty
+                {"Beauty"}
               </p>
               <p className="px-6 py-2 ml-4 rounded-full bg-[var(--tertiary-color)]">
-                Art
+                {"Art"}
               </p>
             </div>
           </div>
-          <div className="border-black border-1 flex h-[60%]">
-            <div className="border-black border-1 w-[50%] h-[80%]">
+
+          {/* --------------------------Image grid-------------------------- */}
+
+          <div className=" flex h-max w-[95%] justify-between items-center">
+            <div className=" w-max h-max">
               <img
                 src=""
                 alt=""
@@ -42,8 +45,8 @@ const Service = () => {
               />
             </div>
 
-            <div className="border-black border-1 w-[50%] flex">
-              <div className="border-black border-1">
+            <div className=" w-[51.5%] h-[327px] flex justify-between">
+              <div className=" flex flex-col justify-between">
                 <img
                   src=""
                   alt=""
@@ -55,7 +58,7 @@ const Service = () => {
                   className="size-[152px] bg-[var(--tertiary-color)] rounded-xl"
                 />
               </div>
-              <div className="border-black border-1">
+              <div className=" flex flex-col justify-between">
                 <img
                   src=""
                   alt=""
@@ -70,7 +73,7 @@ const Service = () => {
             </div>
           </div>
 
-          <div className="border-black border-1 h-[20%]">
+          <div className=" h-[20%]">
             <p className="font-bold my-4 text-lg">Description</p>
             <p className=" wrap-normal w-[80%]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
@@ -84,51 +87,55 @@ const Service = () => {
 
         {/* ------------------------------------------------------- */}
 
-        <div className="border-black border-1 w-[50%] h-full">
-          <ul>
-            <li>
-              <p>Price type</p>
-              <p>Fixed</p>
+        <div className=" w-[30%] h-[80%] flex flex-col items-end justify-between">
+          <ul className=" flex justify-between w-full">
+            <li className="text-center">
+              <p className="mb-2">Price type</p>
+              <p className="font-bold font-[Lexend]">Fixed</p>
             </li>
-            <li>
-              <p>Price type</p>
-              <p>Fixed</p>
+            <li className="text-center">
+              <p className="mb-2">Exp. Time frame</p>
+              <p className="font-bold font-[Lexend]">3 hrs</p>
             </li>
-            <li>
-              <p>Price type</p>
-              <p>Fixed</p>
+            <li className="text-center">
+              <p className="mb-2">Avail. hours</p>
+              <p className="font-bold font-[Lexend]">Sat</p>
             </li>
-            <li>
-              <p>Price type</p>
-              <p>Fixed</p>
+            <li className="text-center">
+              <p className="mb-2">Comp. orders</p>
+              <p className="font-bold font-[Lexend]">33</p>
             </li>
           </ul>
-          <div>
-            <p>
+
+          <div className=" flex w-[70%]">
+            <p className="font-extralight text-md mt-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              eleifend non ipsum vitae egestas. Suspendisse potenti. - by Jude
-              Samuel{" "}
+              eleifend non ipsum vitae egestas. Suspendisse potenti.
+              <br />
+              <span className="font-bold">{"- by Jude Samuel"}</span>
             </p>
-            <img src="" alt="" />
+            <img className="size-10" src={comment} alt="" />
           </div>
-          <div>
-            <textarea name="message" id="message">
-              write a note
+
+          <div className=" bg-[var(--tertiary-color)] w-[80%] h-[140px] flex flex-col justify-between p-4 rounded-2xl">
+            <textarea className="font-extralight" name="message" id="message">
+              Write a note
             </textarea>
-            <img src={back} alt="" />
+            <img className="size-6 scale-x-[-1] self-end" src={back} alt="" />
           </div>
-          <div className="border-black border-1 flex h-[70%] items-end">
-            <img
-              className="border-black border-1 size-16 rounded-full bg-[var(--tertiary-color)] mr-6"
-              src="#"
-              alt=""
-            />
-            <div className="border-black border-1 flex flex-col text-center">
-              <p className="font-semibold text-3xl border-black border-1 mb-2">
-                29,999 <span className="font-extralight text-sm">naira</span>
+
+          <div className=" flex items-end justify-end">
+            <button className="border-black border-3 size-16 rounded-full bg-[var(--tertiary-color)] mr-6 flex justify-center items-center">
+              <img className="size-10" src={cart} alt="" />
+            </button>
+
+            <div className=" flex flex-col text-center">
+              <p className="font-semibold text-3xl mb-2">
+                {"29,999"}{" "}
+                <span className="font-extralight text-sm">naira</span>
               </p>
-              <button className="bg-[var(--sec-color)] text-white px-8 py-4 rounded-full text-2xl">
-                Checkout
+              <button className="bg-[var(--sec-color)] text-white px-10 py-4 rounded-full text-2xl">
+                {"Add to cart"}
               </button>
             </div>
           </div>
